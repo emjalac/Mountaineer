c27fbb99798ef2eec9db25d05fd8ba565fd56e93
Emma Lacroix <emma@Emmas-MacBook-Pro.local>
2018-01-13 14:48:36 -0600

    added collision categories and masks

--- src/States/Game.js
+++ src/States/Game.js
@@ -17,27 +17,45 @@ Mountaineer.Game.prototype = {
     	this.game.physics.box2d.friction = 0.5;
     	this.game.physics.box2d.restitution = 0.5;
 
+	//Initialize vars
+    	var environment;
+    	var ground;
+    	var torso;
+    	var arm_upperfront;
+    	var arm_upperback;
+    	var arm_lowerfront;
+    	var arm_lowerback;
+		var leg_upperfront;
+    	var leg_upperback;
+    	var leg_lowerfront;
+    	var leg_lowerback;
+		var head;
+
     	// Add sprites
-		var ground = this.game.add.sprite(300, 670, "loading-bar");
+	
+	//Environment
+		environment = this.game.add.group();
+    	ground = environment.create(0, 500, "loading-bar");
+    	ground.width = 2000;
 		this.game.physics.box2d.enable(ground);
-		ground.body.immovable = true;
-		ground.width = 1280;
+		ground.body.static = true;
 
-		var torso = this.game.add.sprite(200, 300, "torso");
+		// Player
+		player = this.game.add.group();
+		torso = player.create(200, 300, "torso");
+		torso.enableBody = true;
 		this.game.physics.box2d.enable(torso);
-		torso.body.static = true;
 		torso.body.restitution = 0.3;
-		// torso.body.gravity.y = 500;
 
-		var arm_upperfront = this.game.add.sprite(250, 245, "arm_upperfront");
-		var arm_upperback = this.game.add.sprite(250, 245, "arm_upperback");
-		var arm_lowerfront = this.game.add.sprite(250, 245, "arm_lowerfront");
-		var arm_lowerback = this.game.add.sprite(250, 245, "arm_lowerback");
-		var leg_upperfront = this.game.add.sprite(250, 245, "leg_upperfront");
-		var leg_upperback = this.game.add.sprite(250, 245, "leg_upperback");
-		var leg_lowerfront = this.game.add.sprite(250, 245, "leg_lowerfront");
-		var leg_lowerback = this.game.add.sprite(250, 245, "leg_lowerback");
-		var head = this.game.add.sprite(250, 200, "head");
+		arm_upperfront = this.game.add.sprite(250, 245, "arm_upperfront");
+		arm_upperback = this.game.add.sprite(250, 245, "arm_upperback");
+		arm_lowerfront = this.game.add.sprite(250, 245, "arm_lowerfront");
+		arm_lowerback = this.game.add.sprite(250, 245, "arm_lowerback");
+		leg_upperfront = this.game.add.sprite(250, 245, "leg_upperfront");
+		leg_upperback = this.game.add.sprite(250, 245, "leg_upperback");
+		leg_lowerfront = this.game.add.sprite(250, 245, "leg_lowerfront");
+		leg_lowerback = this.game.add.sprite(250, 245, "leg_lowerback");
+		head = this.game.add.sprite(250, 200, "head");
 		this.game.physics.box2d.enable([arm_upperfront, arm_lowerfront, arm_lowerback, arm_upperback, head, leg_lowerback, leg_upperback, leg_upperfront, leg_lowerfront]);
 		this.game.physics.box2d.revoluteJoint(torso, arm_upperfront, 30, -115, -5, -60, 5, 10, true, -180, 180, true);
 		this.game.physics.box2d.revoluteJoint(arm_upperfront, arm_lowerfront, 0, 60, -5, -70, 5, 10, true, -180, 180, true);
@@ -49,6 +67,37 @@ Mountaineer.Game.prototype = {
 		this.game.physics.box2d.revoluteJoint(torso, leg_upperfront, 35, 100, -5, -60, 5, 10, true);
 		this.game.physics.box2d.revoluteJoint(leg_upperfront, leg_lowerfront, 0, 60, 5, -65, 5, 10, true, -180, 180, true);
 		
+		// Set up collision masks
+		// Player collides with: env (cat, mask) -> (01, 10) -> (1, 2)
+		// Environment collides with: env, player (cat, mask) -> (10, 11) -> (2, 3)
+
+		this.PLAYER_CAT = 1;
+		this.PLAYER_MASK = 2;
+		this.ENV_CAT = 2;
+		this.ENV_MASK = 3;
+
+		torso.body.setCollisionCategory(this.PLAYER_CAT);
+		arm_upperfront.body.setCollisionCategory(this.PLAYER_CAT);
+		arm_upperback.body.setCollisionCategory(this.PLAYER_CAT);
+		arm_lowerfront.body.setCollisionCategory(this.PLAYER_CAT);
+		arm_lowerback.body.setCollisionCategory(this.PLAYER_CAT);
+		leg_upperfront.body.setCollisionCategory(this.PLAYER_CAT);
+		leg_upperback.body.setCollisionCategory(this.PLAYER_CAT);				leg_lowerfront.body.setCollisionCategory(this.PLAYER_CAT);
+		leg_lowerback.body.setCollisionCategory(this.PLAYER_CAT);
+		head.body.setCollisionCategory(this.PLAYER_CAT);
+		ground.body.setCollisionCategory(this.ENV_CAT);
+
+		torso.body.setCollisionMask(this.PLAYER_MASK);
+		arm_upperfront.body.setCollisionMask(this.PLAYER_MASK);
+		arm_upperback.body.setCollisionMask(this.PLAYER_MASK);
+		arm_lowerfront.body.setCollisionMask(this.PLAYER_MASK);
+		arm_lowerback.body.setCollisionMask(this.PLAYER_MASK);
+		leg_upperfront.body.setCollisionMask(this.PLAYER_MASK);
+		leg_upperback.body.setCollisionMask(this.PLAYER_MASK);					leg_lowerfront.body.setCollisionMask(this.PLAYER_MASK);
+		leg_lowerback.body.setCollisionMask(this.PLAYER_MASK);
+		head.body.setCollisionMask(this.PLAYER_MASK);
+		ground.body.setCollisionMask(this.ENV_MASK);
+
 		// Set up handlers for mouse events
 	    this.game.input.onDown.add(this.mouseDragStart, this);
 	    this.game.input.addMoveCallback(this.mouseDragMove, this);
